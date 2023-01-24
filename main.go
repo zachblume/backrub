b10938c03ff518b2906b40a4aa8e3efa3da59ce3
zachblume <zachblume@gmail.com>
2023-01-23 19:28:29 -0500

    struct type for webpages

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+// This is the whole shebang! Collect this for the whole internet and you got a graph
+type Webpage struct {
+	id       int32
+	referrer string // The URL of the referring page, to build the graph
+	url      string
+	title    string
+	pagerank int32
+}
+
 // Startup func
 func main() {
 
