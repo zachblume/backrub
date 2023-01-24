80625c2346b3f214542ea5359cf53f30b8cc4b24
zachblume <zachblume@gmail.com>
2023-01-23 19:20:32 -0500

    file strucutre

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+// Startup func
 func main() {
+
+}
+
+// Takes a URL from queue, and saves a list of every URL it references and the page titles to db
+func scraper() {
+
+}
+
+// Saves row to database
+func save() {
 
 }
